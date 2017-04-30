6d288f125a3f8036c64de8d250584cc86d0980ca
José Moreira <josemoreiravarzim@gmail.com>
2017-04-30 18:30:37 +0100

    comment out auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 export const RRUB_INIT = '@@rrub/INIT'
 export const RRUB_INIT_COMPLETED = '@@rrub/INIT/COMPLETED'
 
-import { authInit } from 'boilerplate/features/auth-backend-rethink/actions'
+// import { authInit } from 'boilerplate/features/auth-backend-rethink/actions'
 import { intlInit } from 'boilerplate/features/intl/actions'
 
 export function init () {
@@ -9,7 +9,7 @@ export function init () {
     await dispatch({ type: RRUB_INIT })
 
     await dispatch( intlInit() )
-    await dispatch( authInit() )
+    // await dispatch( authInit() )
 
     await dispatch({ type: RRUB_INIT_COMPLETED })
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
 import configureReducers from 'boilerplate/resources/configure-reducers'
+// import auth from 'boilerplate/features/auth-backend-rethink/reducers'
 
-export default configureReducers()
+export default configureReducers({
+  // auth
+})
